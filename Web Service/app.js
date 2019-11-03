984e4393854d134c3e08606bc73b5b816612aefa
Oguzhan Kaymak <oguzkaymak3mail.com>
2019-11-03 11:19:05 +0300

    web service config (connection,edit)

---
+++ Web Service/app.js
@@ -0,0 +1,25 @@
+const sql = require('mssql');
+const express = require('express');
+const userRouter = require('./users');
+
+var bodyParser = require('body-parser');
+
+
+const PORT = 5000 || process.env.PORT;
+const app = express();
+
+
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.use(userRouter)
+
+app.get("/", (req, res, next) => {
+    res.send("Oldu");
+})
+
+app.use((req, res, next) => {
+    res.send("404 NOT FOUND");
+})
+app.listen(PORT);
---
+++ Web Service/users.js
@@ -0,0 +1,20 @@
+const sql = require('mssql');
+const express = require('express');
+const router = express();   
+const sqlConfig = require("./routes/dbConfig");
+
+router.post("/login",(req,res,next)=>{
+    new sql.ConnectionPool(sqlConfig).connect()
+    .then(pool =>{
+        return pool.request()
+        .input('TCNo',req.body.TC)
+        .input('Password',req.body.Password)
+        .execute("Login")
+    }).then(result =>{
+        res.send(JSON.stringify(result.returnValue));
+    })
+})
+router.post("/register",(req,res,next) =>{
+    res.send("Register Page");
+})
+module.exports = router;
